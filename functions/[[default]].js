03a00d003721a69d526e662d2ec83c0780857a08
thun888 <thun888@qq.com>
2025-07-21 18:00:02 +0800

    fix

--- functions/[[default]].js
+++ functions/[[default]].js
@@ -1,49 +1,35 @@
 export async function onRequestGet(event) {
-  // event:['request','params','env']
-  // request: ['maxFollow','version','eo','cf','url','referrer','redirect','mode','method','integrity','headers','credentials','cache','bodyUsed','body','destination','referrerPolicy']
-  // params: ['default']
-  // env: 环境变量
   const { request } = event;
   const urlInfo = new URL(request.url);
   const originUrl = `http://emoticons-assets.hzchu.top${urlInfo.pathname}${urlInfo.search}`;
-
+  const cacheKey = new Request(originUrl);
   const cache = await caches.open('cache');
 
-  const cachedResponse = await cache.match(originUrl);
-  console.log(cachedResponse);
+  const cachedResponse = await cache.match(cacheKey);
   if (cachedResponse) {
     console.log('缓存命中');
     return cachedResponse;
   }
 
-  const proxyRequest = new Request(originUrl, {
-    method: request.method,
-    body: request.body,
-    headers: request.headers,
-    cache: 'force-cache',
+  const response = await fetch(cacheKey, {
+    headers: {
+      'Host': 'emoticons-assets.hzchu.top'
+    }
   });
-  proxyRequest.headers.set('Host', 'emoticons-assets.hzchu.top');
 
-
-  // fetch 反向代理
-  const response = await fetch(proxyRequest);
-
-  // 读取content-type
   const contentType = response.headers.get('content-type');
-  /** 删除响应头 **/
+
   const newResponse = new Response(response.body, {
-    headers: new Headers({
+    headers: {
       'Access-Control-Allow-Origin': '*',
       'Access-Control-Allow-Methods': 'GET,POST',
       'Access-Control-Max-Age': '2592000',
       'Cache-Control': 'public,max-age=2592000,immutable',
       'content-type': contentType
-    })
+    }
   });
-  await cache.put(originUrl, newResponse.clone());
 
-
+  await cache.put(cacheKey, newResponse.clone());
   newResponse.headers.append('x-edgefunctions-cache', 'MISS');
-
   return newResponse;
 }
