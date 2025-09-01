d479f3341a206aaacda7baf5e3d5e94fdd8b1a38
thun888 <2238342947@qq.com>
2025-09-01 16:43:32 +0800

    finally fix!

--- functions/[[default]].js
+++ functions/[[default]].js
@@ -1,20 +1,25 @@
 export async function onRequestGet(event) {
   const { request } = event;
   const urlInfo = new URL(request.url);
-  const originUrl = `http://emoticons-assets.hzchu.top${urlInfo.pathname}${urlInfo.search}`;
+  const originUrl = `https://emoticons-cloudflare.hzchu.top${urlInfo.pathname}`;
   const cacheKey = new Request(originUrl);
   const cache = await caches.open('cache');
+  // 如果?delete 则删除缓存
+  if (urlInfo.searchParams.get('delete')) {
+    await cache.delete(cacheKey);
+    return new Response('删除成功', { status: 200 });
+  }
 
   const cachedResponse = await cache.match(cacheKey);
-  if (cachedResponse) {
-    console.log('缓存命中');
+  if (cachedResponse && urlInfo.pathname.startsWith('/emoticons')) {
+    // console.log('缓存命中');
     cachedResponse.headers.append('x-edgefunctions-cache', 'HIT');
     return cachedResponse;
   }
 
   const response = await fetch(cacheKey, {
     headers: {
-      'Host': 'emoticons-assets.hzchu.top'
+      'Host': 'emoticons-cloudflare.hzchu.top'
     }
   });
 
@@ -29,8 +34,9 @@ export async function onRequestGet(event) {
       'content-type': contentType
     }
   });
-
-  await cache.put(cacheKey, newResponse.clone());
+  if (response.status === 200) {
+    await cache.put(cacheKey, newResponse.clone());
+  }
   newResponse.headers.append('x-edgefunctions-cache', 'MISS');
   return newResponse;
 }
